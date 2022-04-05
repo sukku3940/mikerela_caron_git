d31184f84cebd81b79c466587826a78efe286432
SUKUMAR <SUKUMARREDDY4498@GMAIL.COM>
2022-04-05 15:11:17 -0500

    Initial Commit

---
+++ mikerela_git/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mikerela_git
+//
+//  Created by apple on 05/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mikerela_git/mikerela_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  mikerela_gitApp.swift
+//  mikerela_git
+//
+//  Created by apple on 05/04/22.
+//
+
+import SwiftUI
+
+@main
+struct mikerela_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
